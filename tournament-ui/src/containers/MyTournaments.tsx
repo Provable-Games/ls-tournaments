499a73ef4affac6a60b3ec77dda17b2d6cfae53b
Starknet Dev <starknetdev@gmail.com>
2024-12-08 22:44:10 +0000

    add my tournaments connect message

--- tournament-ui/src/containers/MyTournaments.tsx
+++ tournament-ui/src/containers/MyTournaments.tsx
@@ -1,6 +1,15 @@
 import CreatedTable from "@/components/myTournaments/CreatedTable";
+import { useAccount } from "@starknet-react/core";
 
 const MyTournaments = () => {
+  const { account } = useAccount();
+  if (!account) {
+    return (
+      <div className="w-full h-full flex items-center justify-center text-4xl uppercase">
+        Please connect wallet
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col gap-5 w-full p-4 uppercase no-text-shadow">
       <div className="flex flex-row items-center border border-terminal-green p-2 gap-2 uppercase">
