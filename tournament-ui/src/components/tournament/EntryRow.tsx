d1107a284930b0526a6fcfab1889cae48b6fbcdf
Starknet Dev <starknetdev@gmail.com>
2025-01-23 13:29:26 +0000

    add starkname fetcher if no username

--- tournament-ui/src/components/tournament/EntryRow.tsx
+++ tournament-ui/src/components/tournament/EntryRow.tsx
@@ -1,4 +1,5 @@
 import { displayAddress } from "@/lib/utils";
+import { useStarkName } from "@starknet-react/core";
 
 // First, define the props interface
 interface EntryRowProps {
@@ -8,10 +9,13 @@ interface EntryRowProps {
 }
 
 const EntryRow = ({ address, username, entryCount }: EntryRowProps) => {
+  const { data: starkName } = useStarkName({
+    address: address as `0x${string}`,
+  });
   return (
     <tr className="h-8 hover:bg-terminal-green/50 hover:cursor-pointer border border-terminal-green/50">
       <td className="px-2 text-lg uppercase">
-        {username ? username : displayAddress(address)}
+        {username ? username : starkName ? starkName : displayAddress(address)}
       </td>
       <td className="text-lg">{BigInt(entryCount).toString() ?? "-"}</td>
     </tr>
